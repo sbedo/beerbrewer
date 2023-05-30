d3846d678d92b3dd557d1094d7bb0e112d549734
Sandor Bedo <sandor_bedo@epam.com>
2023-05-30 17:32:03 +0200

    initial implementation of i2c driver

---
+++ components/i2c_handler/i2c_handler.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "i2c_handler.h"
+#include "driver_i2c.h"
+
+void func(void)
+{
+    i2c_init();
+}
---
+++ components/i2c_master_driver/component.mk
@@ -0,0 +1,4 @@
+#
+# "i2c_master_driver" component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
